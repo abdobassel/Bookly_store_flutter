778a0f033bc0d568b6ac72e65cc1f9a9d9fae9c5
abdobassel <abdobassel@gmail.com>
2024-01-03 20:58:36 +0200

    create homerepo  and failure for errors

---
+++ lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:book_store/core/errors/failure.dart';
+import 'package:book_store/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  // what is will happening in home feautre ..
+  Future<Either<Failure, List<BookEntity>>> fetchFeautredBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewsedBooks();
+}
