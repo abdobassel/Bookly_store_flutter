9b2645d3d8ddd023dabb4c7c53d904b160388387
abdobassel <abdobassel@gmail.com>
2024-01-03 02:21:00 +0200

    Search listview done

--- lib/features/Search/presintation/widgets/search_body.dart
+++ lib/features/Search/presintation/widgets/search_body.dart
@@ -1,4 +1,6 @@
+import 'package:book_store/core/utilis/styles.dart';
 import 'package:book_store/features/Search/presintation/widgets/custom_text_field_search.dart';
+import 'package:book_store/features/home/presintation/views/widgets/bestSeller_item.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,6 +10,30 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.symmetric(horizontal: 25),
-        child: Column(children: [CustomSearchField()]));
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          CustomSearchField(),
+          Text(
+            'Search Results',
+            style: Styles.textStyle22,
+          ),
+          Expanded(child: searchListViewBooks())
+        ]));
+  }
+}
+
+class searchListViewBooks extends StatelessWidget {
+  const searchListViewBooks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 15,
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: BestSellerListItem(),
+      ),
+    );
   }
 }
