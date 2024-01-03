91076a717a1db5c1f13927c7e504feef52122567
abdobassel <abdobassel@gmail.com>
2024-01-03 21:56:41 +0200

    refactor 2 created file  main use_case as parent to all usecases classes

--- lib/features/home/domain/use_cases/fetch_feautred_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_feautred_books_use_case.dart
@@ -1,26 +1,19 @@
 import 'package:book_store/core/errors/failure.dart';
+import 'package:book_store/core/use_cases/use_case.dart';
 import 'package:book_store/features/home/domain/entities/book_entity.dart';
 import 'package:book_store/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeautredBooksUseCase extends UseCase<List<BookEntity>> {
+class FetchFeautredBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeautredBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> call() {
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
     return homeRepo.fetchFeautredBooks();
   }
 }
 
-abstract class UseCase<Type> {
-  Future<Either<Failure, Type>> call();
-}
-/*
-  FetchFeautredBooksUseCase(this.homeRepo);
-
-  Future<Either<Failure, List<BookEntity>>> call() {
-    return homeRepo.fetchFeautredBooks();
-  }
-   */
+// you can write void replace NoParam
+class NoParam {}
