f89326f88a3a013e875a67238d9b8a42192a221e
abdobassel <abdobassel@gmail.com>
2024-01-03 21:45:24 +0200

    refactor 1 usecase class

--- lib/features/home/domain/use_cases/fetch_feautred_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_feautred_books_use_case.dart
@@ -3,12 +3,24 @@ import 'package:book_store/features/home/domain/entities/book_entity.dart';
 import 'package:book_store/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeautredBooksUseCase {
+class FetchFeautredBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
-  FetchFeautredBooksUseCase({required this.homeRepo});
+  FetchFeautredBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeautredBooks() {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() {
     return homeRepo.fetchFeautredBooks();
   }
 }
+
+abstract class UseCase<Type> {
+  Future<Either<Failure, Type>> call();
+}
+/*
+  FetchFeautredBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> call() {
+    return homeRepo.fetchFeautredBooks();
+  }
+   */
