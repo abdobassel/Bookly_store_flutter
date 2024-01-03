0264eb6056c3a535fc8fc5080ae303b25992f40d
abdobassel <abdobassel@gmail.com>
2024-01-04 00:34:55 +0200

    book model extends bookentity super constructor

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,9 +1,11 @@
+import 'package:book_store/features/home/domain/entities/book_entity.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity {
   String? kind;
   String? id;
   String? etag;
@@ -22,7 +24,13 @@ class BookModel {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+            image: volumeInfo?.imageLinks?.thumbnail ?? '',
+            bookId: id!,
+            title: volumeInfo!.title!,
+            authorName: volumeInfo!.authors!.first,
+            price: 0.0,
+            rating: volumeInfo.averageRating);
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
