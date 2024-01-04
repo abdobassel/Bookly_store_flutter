a274f02e83fee082e325e65f2358d2e7475aadf2
abdobassel <abdobassel@gmail.com>
2024-01-04 23:11:27 +0200

    create method fetchfeautredbooks

---
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -0,0 +1,32 @@
+import 'package:book_store/core/utilis/api_service.dart';
+import 'package:book_store/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeautredBooks();
+  Future<List<BookEntity>> fetchNewsedBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+  @override
+  Future<List<BookEntity>> fetchFeautredBooks() async {
+    var data = await apiService.getData(
+        endPoint: 'volumes?q=programming&Filtering=free-ebooks');
+
+    List<BookEntity> books = [];
+
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewsedBooks() {
+    // TODO: implement fetchNewsedBooks
+    throw UnimplementedError();
+  }
+}
