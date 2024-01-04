7166d83da85ac3898964a53b33f6c5f23c7be945
abdobassel <abdobassel@gmail.com>
2024-01-04 23:21:07 +0200

    fetch newest books and finish remotedatasource

--- lib/features/home/data/data_source/home_remote_data_source.dart
+++ lib/features/home/data/data_source/home_remote_data_source.dart
@@ -20,6 +20,15 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     return books;
   }
 
+  @override
+  Future<List<BookEntity>> fetchNewsedBooks() async {
+    var data = await apiService.getData(
+        endPoint: 'volumes?q=programming&Filtering=free-ebooks&Sorting=newest');
+
+    List<BookEntity> books = getBookList(data);
+    return books;
+  }
+
   List<BookEntity> getBookList(Map<String, dynamic> data) {
     List<BookEntity> books = [];
 
@@ -27,11 +36,5 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
-  }
-
-  @override
-  Future<List<BookEntity>> fetchNewsedBooks() {
-    // TODO: implement fetchNewsedBooks
-    throw UnimplementedError();
   }
 }
